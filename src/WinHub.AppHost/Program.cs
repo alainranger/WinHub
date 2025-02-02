a0a551a10866643f89251cbb9edab70fb75b2ccd
Alain Ranger <alainranger@gmail.com>
2025-02-02 16:02:55 -0500

    Remove unused files and add new Blazor components for improved structure

--- src/WinHub.AppHost/Program.cs
+++ src/WinHub.AppHost/Program.cs
@@ -12,19 +12,9 @@ var apiService = builder.AddProject<Projects.WinHub_ApiService>("apiservice")
 	.WithReference(database)
 	.WaitFor(database);
 
-// Setip Angular Frontend
-builder.AddNpmApp("angular", "../WinHub.Web.Angular")
-	.WithReference(apiService)
-	.WithHttpEndpoint(env: "PORT")
-	.WithExternalHttpEndpoints()
-	.PublishAsDockerFile();
-
 // Setup Blazor frontend
-// builder.AddProject<Projects.WinHub_Web>("webfrontend")
-// 	.WithExternalHttpEndpoints()
-#pragma warning disable S125 // Sections of code should not be commented out
-// 	.WithReference(apiService);
-
+builder.AddProject<Projects.WinHub_Blazor>("webfrontend")
+	.WithExternalHttpEndpoints()
+	.WithReference(apiService);
 
 await builder.Build().RunAsync().ConfigureAwait(true);
-#pragma warning restore S125 // Sections of code should not be commented out
