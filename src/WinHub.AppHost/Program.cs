fa436e36d42267be39a0d9efaa3432604dd44fc3
Alain Ranger <alainranger@gmail.com>
2025-02-05 22:11:06 -0500

    Initialize Vue.js frontend with Vite, including basic structure, routing, and components

--- src/WinHub.AppHost/Program.cs
+++ src/WinHub.AppHost/Program.cs
@@ -8,19 +8,19 @@ var postgres = builder.AddPostgres("postgres")
 var database = postgres.AddDatabase(dbName);
 
 // Setup backend
-var apiService = builder.AddProject<Projects.WinHub_ApiService>("apiservice")
+var apiService = builder.AddProject<Projects.WinHub_ApiService>("backend-apiservice")
 	.WithReference(database)
 	.WaitFor(database);
 
 // Setup Blazor frontend
-builder.AddProject<Projects.WinHub_Blazor>("blazor")
+builder.AddProject<Projects.WinHub_Blazor>("frontend-blazor")
 	.WithReference(apiService)
 	.WaitFor(apiService)
 	.WithExternalHttpEndpoints()
 	.WithReference(apiService);
 
 // Setup React frontend
-builder.AddNpmApp("react", "../frontend/WinHub.React")
+builder.AddNpmApp("frontend-react", "../frontend/WinHub.React")
 	.WithReference(apiService)
 	.WaitFor(apiService)
 	.WithEnvironment("BROWSER", "none") // Disable opening browser on npm start
@@ -29,7 +29,15 @@ builder.AddNpmApp("react", "../frontend/WinHub.React")
 	.PublishAsDockerFile();
 
 // Setup Angular frontend
-builder.AddNpmApp("angular", "../frontend/WinHub.Angular")
+builder.AddNpmApp("frontend-angular", "../frontend/WinHub.Angular")
+	.WithReference(apiService)
+	.WaitFor(apiService)
+	.WithHttpEndpoint(env: "PORT")
+	.WithExternalHttpEndpoints()
+	.PublishAsDockerFile();
+
+// Setup VueJS frontend
+builder.AddNpmApp("frontend-vue", "../frontend/WinHub.VueJS")
 	.WithReference(apiService)
 	.WaitFor(apiService)
 	.WithHttpEndpoint(env: "PORT")
